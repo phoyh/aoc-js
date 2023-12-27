72c40a3ae9c0f95778d6faf4be5a3c7e96ce4d5c
phoyh <59613851+phoyh@users.noreply.github.com>
2023-12-27 08:48:46 +0100

    plainly replaced input parser's EOL with \\n, input should never be checked out with crlf

--- api/parse.js
+++ api/parse.js
@@ -1,8 +1,6 @@
-const { EOL } = require('os');
-
 module.exports = (filePath) => {
 	var inputRaw = require('fs').readFileSync('./input/' + filePath + '.txt', 'utf8');
-	var result = inputRaw.split(EOL + EOL).map(part => part.split(EOL).filter(l => l))
+	var result = inputRaw.split('\n\n').map(part => part.split('\n').filter(l => l))
 		.map(partArray => partArray.length === 1 ? partArray[0] : partArray);
 	return result.length === 1 ? result[0] : result;
 };
